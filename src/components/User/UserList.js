c1a71aefe0654252769d14f8e3178c432ec76230
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-16 11:22:18 +0400

    added userList

---
+++ src/components/User/UserList.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function UserList() {
+  const [Users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://reqres.in/api/users?page=1")
+      .then((response) => {
+        setUsers(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch((err) => {
+        console.error("[UserList.js]", err.message);
+      });
+  }, []);
+
+  return <div></div>;
+}
+
+export default UserList;
